b22e800c654d13147fb1c9fb911a9c0d3db6dbed
duxlong <duxlong@me.com>
2020-11-22 14:50:52 +0800

    Update best-cf-ip.sh

--- best-cf-ip.sh
+++ best-cf-ip.sh
@@ -16,32 +16,24 @@ echo "$(date) 开始运行"
 # 等待 NAS 开机完成，否则测速不准
 sleep 10
 
-# 通过两次测速判断 current ip 是否满足要求
+# 测速判断【目前IP】是否满足要求
+# 通过下载 https://speed.cloudflare.com/__down?bytes=100000000 这个 95.4MB 的文件，判断 IP 速度
 # [ -s $file ] 判断文件不为空
 if [ -s ip.txt ]; then
     echo "测速目前 IP 的速度"
     ip=$(cat ip.txt)
-    speed=$(($(curl --resolve speed.cloudflare.com:443:$ip https://speed.cloudflare.com/__down?bytes=1000000000 -o /dev/null -s -w '%{speed_download}\n' --connect-timeout 5 --max-time 15 | sed "s/.000//") / 1024 / 1024 * 8))
+    speed=$(($(curl --resolve speed.cloudflare.com:443:$ip https://speed.cloudflare.com/__down?bytes=100000000 -o /dev/null -s -w '%{speed_download}\n' --connect-timeout 5 --max-time 15 | sed "s/.000//") / 1024 / 1024 * 8))
     if [ $speed -gt $base_speed ]; then
-        echo "目前 IP $ip 速度 ${speed}Mb/s 大于 ${base_speed}Mb/s"
+        echo "目前 IP $ip , 速度 ${speed}Mb/s , 已超过 ${base_speed}Mb/s , 满足要求"
         echo "$(date) 结束运行"
         exit 0
     else
-        higher_speed=$speed
-        speed=$(($(curl --resolve apple.freecdn.workers.dev:443:$ip https://apple.freecdn.workers.dev/105/media/us/iphone-11-pro/2019/3bd902e4-0752-4ac1-95f8-6225c32aec6d/films/product/iphone-11-pro-product-tpl-cc-us-2019_1280x720h.mp4 -o /dev/null -s -w '%{speed_download}\n' --connect-timeout 5 --max-time 15 | sed "s/.000//") / 1024 / 1024 * 8))
-        if [ $speed -gt $base_speed ]; then
-            echo "目前 IP $ip 速度 ${speed}Mb/s 大于 ${base_speed}Mb/s"
-            echo "$(date) 结束运行"
-            exit 0
-        fi
-        if [ $speed -gt $higher_speed ]; then higher_speed=$speed; fi
+        echo "目前 IP $ip , 速度 ${speed}Mb/s , 未达到 ${base_speed}Mb/s , 不满足要求 , 寻找新 IP"
+        # 记录目前 IP 的信息，用于后面作对比
+        current_speed=$speed
+        current_ip=$ip
     fi
-    current_ip=$ip
-    current_speed=$higher_speed
-    echo "目前 IP $current_ip 速度 ${current_speed}Mb/s"
 fi
-
-echo "目前 IP 不满足要求，寻找新 IP"
 
 rm -rf /tmp/*
 
@@ -51,6 +43,7 @@ for ip in $(cat ip-core.txt); do
     echo $ip | sed "s/.$/$r/" >>/tmp/ip-random.txt
 done
 
+# 筛选 ping 最小的 100 个 IP
 echo "ip-random.txt to ip-100.txt"
 # 2>/dev/null 不显示错误提示
 fping -f /tmp/ip-random.txt -c $fping_count -i 0 2>/dev/null | grep "\[$(($fping_count - 1))\]" | sort -n -k 10 | head -100 >/tmp/ip-100.txt
@@ -62,39 +55,40 @@ if [ ! -s /tmp/ip-100.txt ]; then
     exit 1
 fi
 
-echo "ip-100.txt to ip-3.txt"
+# 统一测速 100 个 IP，筛选下载速度最快的 5 个 IP
+echo "ip-100.txt to ip-5.txt"
 for ip in $(cat /tmp/ip-100.txt | awk '{print $1}'); do
-    curl --resolve speed.cloudflare.com:443:$ip https://speed.cloudflare.com/__down?bytes=1000000000 -o /tmp/$ip -s --connect-timeout 2 --max-time 10 &
+    curl --resolve speed.cloudflare.com:443:$ip https://speed.cloudflare.com/__down?bytes=100000000 -o /tmp/$ip -s --connect-timeout 2 --max-time 10 &
     sleep 0.5
 done
 sleep 10
-ls -S /tmp | head -3 >/tmp/ip-3.txt
+ls -S /tmp | head -5 >/tmp/ip-5.txt
 
-# 对最快的三个 ip 按两种方式独立测速，准确性较高
-echo "ip-3.txt to ip-6.txt"
-for ip in $(cat /tmp/ip-3.txt); do
-    curl --resolve speed.cloudflare.com:443:$ip https://speed.cloudflare.com/__down?bytes=1000000000 -o /dev/null -s -w '%{speed_download}\n' --connect-timeout 5 --max-time 15 | sed "s/.000/\t$ip/" >>/tmp/ip-6.txt
-    sleep 0.5
-    curl --resolve apple.freecdn.workers.dev:443:$ip https://apple.freecdn.workers.dev/105/media/us/iphone-11-pro/2019/3bd902e4-0752-4ac1-95f8-6225c32aec6d/films/product/iphone-11-pro-product-tpl-cc-us-2019_1280x720h.mp4 -o /dev/null -s -w '%{speed_download}\n' --connect-timeout 5 --max-time 15 | sed "s/.000/\t$ip/" >>/tmp/ip-6.txt
+# 对最快的 5 个 IP 独立测速，准确性较高，找到最快的 IP
+echo "ip-5.txt to ip.txt"
+for ip in $(cat /tmp/ip-5.txt); do
+    curl --resolve speed.cloudflare.com:443:$ip https://speed.cloudflare.com/__down?bytes=100000000 -o /dev/null -s -w '%{speed_download}\n' --connect-timeout 5 --max-time 15 | sed "s/.000/\t$ip/" >>/tmp/ip-last.txt
     sleep 0.5
 done
 
-echo "ip-6.txt to ip.txt"
-last=$(cat /tmp/ip-6.txt | sort -r -n -k 1 | head -1)
+# 处理 ip-last.txt 得到结果
+last=$(cat /tmp/ip-last.txt | sort -r -n -k 1 | head -1)
 last_ip=$(echo $last | awk '{print $2}')
 last_speed=$(($(echo $last | awk '{print $1}') / 1024 / 1024 * 8))
+
 # last_speed vs current_speed
 if [ $current_speed -gt $last_speed ]; then
     last_speed=$current_speed
     last_ip=$current_ip
 fi
+
 echo $last_ip >ip.txt
 
 end_seconds=$(date +%s)
 
 echo "$last_ip 满足要求，速度是 ${last_speed}Mb/s，耗时 $(($end_seconds - $start_seconds)) 秒！"
 
-echo "修改 v2ray 配置文件中的 IP"
+echo "修改 v2ray config.json"
 sed -i "s/\(\"address\":\"\)\(.*\)\(\",\)/\1${last_ip}\3/" /root/v2ray/config.json
 
 # $DOCKERNAME : ENV 变量
